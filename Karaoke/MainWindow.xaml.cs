f83ab29fd5264a8269c2900947d3082f0dbf321a
Neto322 <42181276+Neto322@users.noreply.github.com>
2020-01-25 13:48:24 -0600

    Listo

--- Karaoke/MainWindow.xaml.cs
+++ Karaoke/MainWindow.xaml.cs
@@ -38,14 +38,50 @@ namespace Karaoke
 
         int[] second =
         {
-            0,3000,6000
+            0,3200,7100,11107,14112,18006,22012,25090,29002,33000,36500,40000,44000,47000,52000,55000,57000,61000,63000,65000,72000,75000,79000,82000,87000,90000,93000,101000,10000000
 
         };
 
         string[] lyrics =
         {
-            "A mí me gusta el Tangananica ;)",
-            "Y yo prefiero el tanganana :0"
+            "A mí me gusta el Tangananica ^̮^",
+            "Y yo prefiero el tanganana ˙ ͜ʟ˙",
+            "La mejor frase es tangananica ヽ༼ຈل͜ຈ༽ﾉ",
+            @"El mejor verso es tanganana \ (•◡•) /",
+            "Tangananica nica nica nica ~(˘▾˘~)",
+            "Tanganana ヾ(⌐■_■)ノ♪",
+            "Todo lo explica ja!! (ᵔᴥᵔ)",
+            "no explica na(╯°□°)╯",
+            "Para alegrarme digo tangananica ヽ(◕ヮ◕ヽ)",
+            "Para reírme digo tanganana (☞ﾟ∀ﾟ)☞",
+            "Comí un rico tangananica(づ￣ ³￣)づ",
+            "A mí me dieron tanganana ( ͡° ͜ʖ ͡°)",
+            "Tangananica nica nica nica (◕‿◕✿)",
+            "Tanganana (¬‿¬)",
+            "Me tienes pica pica (;´༎ຶД༎ຶ`)",
+            "no pasa na ( ▀ ͜͞ʖ▀)",
+            "Cómo voy a perder mi palabra es tan buena ( ͡ᵔ ͜ʖ ͡ᵔ )",
+            "Tu palabra es tan mala ◉_◉",
+            @"no hay nada qué hacer¯\_(ツ)_/¯",
+            "Cómo vas a ganar si la mejor palabra es tanganana (ง ͠° ͟ل͜ ͡°)ง",
+            "Tú siempre dices tangananica ಠ_ಠ",
+            "Tú siempre gritas tanganana ಠ╭╮ಠ",
+            "Ya no soporto el tangananicaლ(ಠ益ಠლ)",
+            "Y yo detesto tu tanganana(¬_¬)",
+            "Tangananica nica nica nica ╚(ಠ_ಠ)=┐",
+            "Tanganana ლ,ᔑ•ﺪ͟͠•ᔐ.ლ",
+            "Nuestra mamá nos va a retar... °Д°",
+            "Oye, oye, ordenemos mejor o si no nos van a pillar..."
+
+
+
+
+
+
+
+
+
+
         };
 
         public MainWindow()
